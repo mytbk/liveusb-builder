09aa7ff772730824e40c7162c2d2d58bcbec4070
Iru Cai <mytbk920423@gmail.com>
2016-07-05 21:15:46 +0800

    update arch install_live()

--- distro/arch/install.sh
+++ distro/arch/install.sh
@@ -3,7 +3,11 @@ SHA1=6db5a9e46267ba7ec4d9ae79d141e5a6d9d3cf88
 
 install_live() {
 	mount_iso
-	cp -r "$ROOTPATH"
+	install -d "$DATADIR/arch"
+	cp -r "$ISOMNT/arch/"{i686,x86_64} "$DATADIR/arch/"
+	install -d "$KERNELDIR/arch"
+	cp -r "$ISOMNT/arch/boot/"{i686,x86_64} "$KERNELDIR/arch/"
+	cp "$ISOMNT/arch/boot/intel_ucode.img" "$KERNELDIR/arch/"
 	umount_iso
 }
 
