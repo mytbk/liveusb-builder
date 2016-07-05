7be29a553f8bc94df3a6d67c497faa2371913dce
Iru Cai <mytbk920423@gmail.com>
2016-07-05 20:34:43 +0800

    install() -> install_live()

--- distro/arch/install.sh
+++ distro/arch/install.sh
@@ -1,8 +1,9 @@
 ISOURL=iso/2016.08.01/archlinux-2016.08.01-dual.iso
 SHA1=6db5a9e46267ba7ec4d9ae79d141e5a6d9d3cf88
 
-install() {
+install_live() {
 	mount_iso
+	cp -r "$ROOTPATH"
 	umount_iso
 }
 
--- distro/fedora/install.sh
+++ distro/fedora/install.sh
@@ -1,7 +1,7 @@
 ISOURL=releases/24/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-24-1.2.iso
 SHA256=8e12d7ba1fcf3328b8514d627788ee0146c0eef75a5e27f0674ee1fe4f1feaf6
 
-install() {
+install_live() {
 	mount_iso
 	umount_iso
 }
