0da1c12172eb773c2d86bbe7215afb02b3d50884
Iru Cai <mytbk920423@gmail.com>
2016-08-02 20:15:36 +0800

    distro/fedora: add grub.cfg and mirrorlist, update install.sh

--- distro/fedora/install.sh
+++ distro/fedora/install.sh
@@ -3,6 +3,10 @@ SHA256=8e12d7ba1fcf3328b8514d627788ee0146c0eef75a5e27f0674ee1fe4f1feaf6
 
 install_live() {
 	mount_iso
+	install -d "$DATADIR/fedora"
+	cp "$ISOMNT/LiveOS"/* "$DATADIR/fedora/"
+	install -d "$KERNELDIR/fedora"
+	cp "$ISOMNT/isolinux/vmlinuz" "$ISOMNT/isolinux/initrd.img" "$KERNELDIR/fedora"
 	umount_iso
 }
 
