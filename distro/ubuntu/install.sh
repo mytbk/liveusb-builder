de9aae7a73d83d6854548e5a147f929daf6f1633
Iru Cai <mytbk920423@gmail.com>
2016-08-10 17:38:28 +0800

    remove $DATADIR/$distro/64

--- distro/ubuntu/install.sh
+++ distro/ubuntu/install.sh
@@ -1,5 +1,5 @@
 install_live() {
-	install -d "$KERNELDIR/ubuntu/64" "$DATADIR/ubuntu/64"
+	install -d "$KERNELDIR/ubuntu/64"
 	cp "isofiles/$ISOFILE" "$DATADIR/ubuntu/"
 	mount_iso
 	cp "$ISOMNT/casper/vmlinuz.efi" "$ISOMNT/casper/initrd.lz" "$KERNELDIR/ubuntu/64/"
