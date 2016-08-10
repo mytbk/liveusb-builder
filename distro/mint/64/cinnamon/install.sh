634ae0c4b0d165609296b5e81d56d4f186480ff9
Iru Cai <mytbk920423@gmail.com>
2016-08-10 17:10:22 +0800

    distro/mint: add Xfce and Cinnamon desktop
    - Move mint/mate/64 as mint/64/mate because
      64-bit and 32-bit is a more significant difference than desktop
    - The kernel of Xfce and MATE is identical and the initrd.lz is
      almost identical, it's OK to use the same vmlinuz and initrd.lz
      for both ISOs
    - remove the creation of $DATADIR/mint/64

--- distro/mint/64/cinnamon/install.sh
+++ distro/mint/64/cinnamon/install.sh
@@ -1,5 +1,5 @@
 install_live() {
-	install -d "$KERNELDIR/mint/64" "$DATADIR/mint/64"
+	install -d "$KERNELDIR/mint/64"
 	cp "isofiles/$ISOFILE" "$DATADIR/mint/"
 	mount_iso
 	cp "$ISOMNT/casper/vmlinuz" "$ISOMNT/casper/initrd.lz" "$KERNELDIR/mint/64/"
