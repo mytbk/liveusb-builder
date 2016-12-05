d4c818b818bac8960c7622af00be2500adb3a613
Iru Cai <mytbk920423@gmail.com>
2016-12-05 14:03:00 +0800

    distro/fedora: Add Fedora 25, and fix the GRUB entry

---
+++ distro/fedora/24/install.sh
@@ -0,0 +1,3 @@
+version=24
+source distro/fedora/install.sh
+
--- distro/fedora/install.sh
+++ distro/fedora/install.sh
@@ -1,9 +1,8 @@
 install_live() {
 	mount_iso
-	install -d "$DATADIR/fedora"
-	cp "$ISOMNT/LiveOS"/* "$DATADIR/fedora/"
-	install -d "$KERNELDIR/fedora"
-	cp "$ISOMNT/isolinux/vmlinuz" "$ISOMNT/isolinux/initrd.img" "$KERNELDIR/fedora"
+	install -d "$DATADIR/fedora/$version" "$KERNELDIR/fedora/$version"
+	cp "$ISOMNT/LiveOS"/* "$DATADIR/fedora/$version"
+	cp "$ISOMNT/isolinux/vmlinuz" "$ISOMNT/isolinux/initrd.img" "$KERNELDIR/fedora/$version"
 	umount_iso
 }
 
