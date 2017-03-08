28cf5ab3e6d43ed4b9773c768be96102da8629fb
Iru Cai <mytbk920423@gmail.com>
2017-03-08 17:16:23 +0800

    distro/arch: update to 2017.03.01

--- distro/arch/install.sh
+++ distro/arch/install.sh
@@ -1,9 +1,9 @@
 install_live() {
 	mount_iso
 	install -d "$DATADIR/arch"
-	cp -r "$ISOMNT/arch/"{i686,x86_64} "$DATADIR/arch/"
+	cp -r "$ISOMNT/arch/x86_64" "$DATADIR/arch/"
 	install -d "$KERNELDIR/arch"
-	cp -r "$ISOMNT/arch/boot/"{i686,x86_64} "$KERNELDIR/arch/"
+	cp -r "$ISOMNT/arch/boot/x86_64" "$KERNELDIR/arch/"
 	cp "$ISOMNT/arch/boot/intel_ucode.img" "$KERNELDIR/arch/"
 	umount_iso
 }
