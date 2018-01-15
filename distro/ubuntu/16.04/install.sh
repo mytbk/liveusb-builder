fb7dea02cd8d58d8942e30c6dbc51594ced56eb9
Iru Cai <mytbk920423@gmail.com>
2018-01-15 15:47:31 +0800

    add ubuntu 14.04.5 LTS

---
+++ distro/ubuntu/16.04/install.sh
@@ -0,0 +1,8 @@
+install_live() {
+	install -d "$KERNELDIR/ubuntu/16.04/64" "$DATADIR/ubuntu"
+	cp "isofiles/$ISOFILE" "$DATADIR/ubuntu/"
+	mount_iso
+	cp "$ISOMNT/casper/vmlinuz.efi" "$ISOMNT/casper/initrd.lz" "$KERNELDIR/ubuntu/16.04/64/"
+	umount_iso
+}
+
--- distro/ubuntu/install.sh
+++
@@ -1,8 +0,0 @@
-install_live() {
-	install -d "$KERNELDIR/ubuntu/64" "$DATADIR/ubuntu"
-	cp "isofiles/$ISOFILE" "$DATADIR/ubuntu/"
-	mount_iso
-	cp "$ISOMNT/casper/vmlinuz.efi" "$ISOMNT/casper/initrd.lz" "$KERNELDIR/ubuntu/64/"
-	umount_iso
-}
-
