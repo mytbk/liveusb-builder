2dd63bfc914e6737089dd93cad7adf61dd3f9a4c
Iru Cai <mytbk920423@gmail.com>
2018-01-25 16:43:51 +0800

    gloriousarch: update, add nox version, iomem=relaxed

---
+++ distro/gloriousarch/nox/install.sh
@@ -0,0 +1,11 @@
+_name=garch/nox
+install_live() {
+	mount_iso
+	install -d "$DATADIR/$_name"
+	install -d "$KERNELDIR/$_name"
+	cp -r "$ISOMNT/arch/x86_64" "$DATADIR/$_name/"
+	cp -r "$ISOMNT/arch/boot/x86_64" "$KERNELDIR/$_name/"
+	cp "$ISOMNT/arch/boot/intel_ucode.img" "$KERNELDIR/$_name/"
+	umount_iso
+}
+
