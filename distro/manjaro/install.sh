1478a0195a53233c8343777703e8a94d1fdb23d1
Iru Cai <mytbk920423@gmail.com>
2018-09-02 10:21:50 +0800

    make manjaro work

--- distro/manjaro/install.sh
+++ distro/manjaro/install.sh
@@ -1,7 +1,7 @@
 install_live() {
 	install -d "$DATADIR/manjaro/" "$KERNELDIR/manjaro"
 	mount_iso
-	cp -r "$ISOMNT/manjaro/boot/$ARCH" "$ISOMNT/manjaro/boot/intel_ucode.img" \
+	cp "$ISOMNT/boot/vmlinuz-$ARCH" "$ISOMNT/boot/initramfs-$ARCH.img" "$ISOMNT/boot/intel_ucode.img" \
 	       	"$KERNELDIR/manjaro"
 	umount_iso
         cp "$ISOPATH/$ISOFILE" "$DATADIR/manjaro/"
