ddef836780ad28cd41ba543475145421683bb715
Iru Cai <mytbk920423@gmail.com>
2018-12-03 00:05:09 +0800

    update gloriousarch/xfce

--- distro/gloriousarch/xfce/install.sh
+++ distro/gloriousarch/xfce/install.sh
@@ -1,11 +1,3 @@
-_name=garch/xfce
-install_live() {
-	mount_iso
-	install -d "$DATADIR/$_name"
-	install -d "$KERNELDIR/$_name"
-	cp -r "$ISOMNT/arch/x86_64" "$DATADIR/$_name/"
-	cp -r "$ISOMNT/arch/boot/x86_64" "$KERNELDIR/$_name/"
-	cp "$ISOMNT/arch/boot/intel_ucode.img" "$KERNELDIR/$_name/"
-	umount_iso
-}
-
+# dirty hack...
+KEYWORD=$KEYWORD/xfce
+. distro/arch/install.sh
