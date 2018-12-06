351e53aec275e71b9a4ae5db4b0986d60b482b6d
Iru Cai <mytbk920423@gmail.com>
2018-12-06 16:08:05 +0800

    distro/debian: fix file name of kernel images

--- distro/debian/64/install.sh
+++ distro/debian/64/install.sh
@@ -2,7 +2,7 @@ install_live() {
 	install -d "$KERNELDIR/debian/64" "$DATADIR/debian"
 	cp "$ISOPATH/$ISOFILE" "$DATADIR/debian/"
 	mount_iso
-	cp "$ISOMNT/live/vmlinuz" "$ISOMNT/live/initrd.img" "$KERNELDIR/debian/64/"
+	cp "$ISOMNT/live/vmlinuz"* "$ISOMNT/live/initrd.img"* "$KERNELDIR/debian/64/"
 	umount_iso
 }
 
