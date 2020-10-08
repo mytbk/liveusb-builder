a9fbcac5a8e4bff84d34659fb3450c749ee1cd6c
Iru Cai <mytbk920423@gmail.com>
2020-10-08 19:15:24 +0800

    distro/arch: use img_dev and img_loop cmdline

--- distro/arch/install.sh
+++ distro/arch/install.sh
@@ -1,8 +1,8 @@
 install_live() {
 	mount_iso
 	install -d "$DATADIR/$KEYWORD" "$KERNELDIR/$KEYWORD"
-	cp -r "$ISOMNT/arch/x86_64" "$DATADIR/$KEYWORD/"
 	cp -r "$ISOMNT/arch/boot/x86_64" "$KERNELDIR/$KEYWORD/"
+	cp "${ISO_FILEPATH}" "$DATADIR/$KEYWORD/"
 	cp "$ISOMNT/arch/boot/"*.img "$KERNELDIR/$KEYWORD/"
 	umount_iso
 }
