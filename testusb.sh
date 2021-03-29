a42acb2e3d3f823e8db5725eb543e74e9cae32bc
Iru Cai <mytbk920423@gmail.com>
2021-03-29 14:01:39 +0800

    testusb.sh: support UEFI

--- testusb.sh
+++ testusb.sh
@@ -3,4 +3,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Test the live USB in QEMU, you need to run this script as root
-qemu-system-x86_64 -enable-kvm -cpu host -m 2G -drive "file=$1,format=raw"
+if [ "$1" == uefi ]; then
+	bios=(-bios /usr/share/edk2-ovmf/x64/OVMF.fd)
+	shift
+else
+	bios=()
+fi
+
+qemu-system-x86_64 -enable-kvm -cpu host -m 2G "${bios[@]}" -drive "file=$1,format=raw"
