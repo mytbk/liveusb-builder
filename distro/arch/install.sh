f48291dec27b4c95f8678c63a5e0ed55d5b29e4b
Iru Cai <mytbk920423@gmail.com>
2021-03-29 12:40:18 +0800

    distro/arch: use iso_extract

--- distro/arch/install.sh
+++ distro/arch/install.sh
@@ -1,9 +1,6 @@
 install_live() {
-	mount_iso
 	install -d "$DATADIR/$KEYWORD" "$KERNELDIR/$KEYWORD"
-	cp -r "$ISOMNT/arch/boot/x86_64" "$KERNELDIR/$KEYWORD/"
+	iso_extract "${ISO_FILEPATH}" arch/boot/x86_64 'arch/boot/*.img' "$KERNELDIR/$KEYWORD/"
 	cp "${ISO_FILEPATH}" "$DATADIR/$KEYWORD/"
-	cp "$ISOMNT/arch/boot/"*.img "$KERNELDIR/$KEYWORD/"
-	umount_iso
 }
 
